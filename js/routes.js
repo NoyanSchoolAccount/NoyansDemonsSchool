d07d8c2e45a7be9c283347b2aa88f7f4be30235f
Noyan <noyannowhasyt@gmail.com>
2025-06-09 23:54:02 +0300

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,21 @@
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
+import Packs from './pages/Packs.js';
+import Grind from './pages/Grind.js'
 
 export default [
-    { path: '/', component: List },
-    { path: '/leaderboard', component: Leaderboard },
-    { path: '/roulette', component: Roulette },
+    // Sets the browser link to access each page
+    { path: "/", component: List },
+    { path: '/level/:level', component: List },
+    
+    { path: "/leaderboard", component: Leaderboard },
+    { path: "/leaderboard/user/:user", component: Leaderboard },
+
+    { path: "/roulette", component: Roulette },
+
+    { path: "/packs", component: Packs },
+    { path: "/packs/pack/:pack", component: Packs },
+
+    { path: "/grind", component: Grind },
 ];
