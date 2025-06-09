deb17891233d71e6e09147c0a2e4398e7230bc76
Noyan <noyannowhasyt@gmail.com>
2025-06-09 23:43:48 +0300

    Update LevelAuthors.js

--- js/components/List/LevelAuthors.js
+++ js/components/List/LevelAuthors.js
@@ -1,9 +1,5 @@
 export default {
     props: {
-        author: {
-            type: String,
-            required: true,
-        },
         creators: {
             type: Array,
             required: true,
@@ -12,48 +8,49 @@ export default {
             type: String,
             required: true,
         },
+        enjoyment: {
+            type: Number,
+            required: false,
+        },
     },
+
     template: `
         <div class="level-authors">
             <template v-if="selfVerified">
                 <div class="type-title-sm">Creator & Verifier</div>
                 <p class="type-body">
-                    <span>{{ author }}</span>
+                    <a class="director link" :href="'https://laylist.pages.dev/#/leaderboard/user/' + creators[0].toLowerCase().replaceAll(' ', '_')">{{ creators[0] }}<span v-if="enjoyment"> ({{ enjoyment }}/10)</span></a>
                 </p>
             </template>
-            <template v-else-if="creators.length === 0">
+            <template v-else-if="creators.length === 1">
                 <div class="type-title-sm">Creator</div>
                 <p class="type-body">
-                    <span>{{ author }}</span>
+                    <a class="director link" :href="'https://laylist.pages.dev/#/leaderboard/user/' + creators[0].toLowerCase().replaceAll(' ', '_')">{{ creators[0] }}</a>
                 </p>
                 <div class="type-title-sm">Verifier</div>
                 <p class="type-body">
-                    <span>{{ verifier }}</span>
+                    <a class="director link" :href="'https://laylist.pages.dev/#/leaderboard/user/' + verifier.toLowerCase().replaceAll(' ', '_')">{{ verifier }}<span v-if="enjoyment"> ({{ enjoyment }}/10)</span></a>
                 </p>
             </template>
             <template v-else>
                 <div class="type-title-sm">Creators</div>
                 <p class="type-body">
-                    <template v-for="(creator, index) in creators" :key="\`creator-\$\{creator\}\`">
-                        <span >{{ creator }}</span
-                        ><span v-if="index < creators.length - 1">, </span>
+                    <template v-for="(creator, index) in creators">
+                        <a class="director link" :href="'https://laylist.pages.dev/#/leaderboard/user/' + creator.toLowerCase().replaceAll(' ', '_')">{{ creator }}</a>
+                        <span v-if="index < creators.length - 1">, </span>
                     </template>
                 </p>
                 <div class="type-title-sm">Verifier</div>
                 <p class="type-body">
-                    <span>{{ verifier }}</span>
+                    <a class="director link" :href="'https://laylist.pages.dev/#/leaderboard/user/' + verifier.toLowerCase().replaceAll(' ', '_')">{{ verifier }}<span v-if="enjoyment"> ({{ enjoyment }}/10)</span></a>
                 </p>
             </template>
-            <div class="type-title-sm">Publisher</div>
-            <p class="type-body">
-                <span>{{ author }}</span>
-            </p>
         </div>
     `,
 
     computed: {
         selfVerified() {
-            return this.author === this.verifier && this.creators.length === 0;
+            return this.creators[0] === this.verifier && this.creators.length === 1;
         },
     },
 };
