0751806d20da3d0e9ba0ccc0527cb56bf87b5095
Noyan <noyannowhasyt@gmail.com>
2025-06-09 23:51:45 +0300

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -1,184 +1,261 @@
-import { store } from "../main.js";
-import { embed } from "../util.js";
-import { score } from "../score.js";
-import { fetchEditors, fetchList } from "../content.js";
-
-import Spinner from "../components/Spinner.js";
-import LevelAuthors from "../components/List/LevelAuthors.js";
-
-const roleIconMap = {
-    owner: "crown",
-    admin: "user-gear",
-    helper: "user-shield",
-    dev: "code",
-    trial: "user-lock",
-};
+import { store } from '../main.js';
+import { averageEnjoyment } from '../content.js';
+import Spinner from '../components/Spinner.js';
+import Scroll from '../components/Scroll.js'
+import Level from '../components/List/Level.js'
+import TierInfo from '../components/List/TierInfo.js';
+import CookiesDisclaimer from '../components/Sidebar/CookiesDisclaimer.js';
+import DifficultyInfo from '../components/Sidebar/DifficultyInfo.js';
+import RecordRules from '../components/Sidebar/RecordRules.js';
+import TemplateDisclaimer from '../components/Sidebar/TemplateDisclaimer.js';
+import Staff from '../components/Sidebar/Staff.js';
+import Errors from '../components/Sidebar/Errors.js';
 
 export default {
-    components: { Spinner, LevelAuthors },
+    components: { Spinner, Scroll, Level, TierInfo, CookiesDisclaimer, RecordRules, DifficultyInfo, TemplateDisclaimer, Staff, Errors },
     template: `
         <main v-if="loading">
             <Spinner></Spinner>
         </main>
         <main v-else class="page-list">
-            <div class="list-container">
-                <table class="list" v-if="list">
-                    <tr v-for="([level, err], i) in list">
-                        <td class="rank">
-                            <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
-                            <p v-else class="type-label-lg">Legacy</p>
-                        </td>
-                        <td class="level" :class="{ 'active': selected == i, 'error': !level }">
-                            <button @click="selected = i">
-                                <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
-                            </button>
-                        </td>
-                    </tr>
-                </table>
+        <div class="list-container">
+            <div class="search-container">
+                <input
+                    type="text"
+                    class="search"
+                    id="search-bar"
+                    placeholder="Search..."
+                    v-model="searchQuery"
+                />
+                <button v-if="searchQuery" @click="searchQuery = ''" class="clear-search">x</button>
             </div>
+            <div class="button-bar" :class="true ? 'dark' : ''">
+                <Scroll alt="Scroll to selected" @click="scrollToSelected()" />
+                <select v-model="sortOption">
+                    <option value="0">Ranking</option>
+                    <option value="1">Enjoyment</option>
+                    <option value="2">Popularity</option>
+                </select>
+                <p style="font-size: 9.5px; opacity: 30%;" class="director" @click="descending = !descending">{{ descending === true ? 'Descending' : 'Ascending' }}</p>
+            </div>
+            <table class="list" v-if="filteredLevels.length > 0">
+                <tr v-for="({ item: [err, rank, level], index }, i) in filteredLevels" :key="index">
+                    <td class="rank">
+                        <p v-if="rank === null" class="type-label-lg" style="width:2.7rem">&mdash;</p>
+                        <p v-else class="type-label-lg" style="width:2.7rem">#{{ rank }}</p>
+                    </td>
+                    <td class="level" :class="{ 'active': selected == index, 'error': err !== null }" :ref="selected == index ? 'selected' : undefined">
+                        <button @click="selected = index">
+                            <span class="type-label-lg">{{ level?.name || 'Error (' + err + '.json)' }}</span>
+                        </button>
+                    </td>
+                </tr>
+            </table>
+            <p class="level" style="padding:1.1rem" v-else>No levels found.</p>
+        </div>
             <div class="level-container">
-                <div class="level" v-if="level">
-                    <h1>{{ level.name }}</h1>
-                    <LevelAuthors :author="level.author" :creators="level.creators" :verifier="level.verifier"></LevelAuthors>
-                    <iframe class="video" id="videoframe" :src="video" frameborder="0"></iframe>
-                    <ul class="stats">
-                        <li>
-                            <div class="type-title-sm">Points when completed</div>
-                            <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
-                        </li>
-                        <li>
-                            <div class="type-title-sm">ID</div>
-                            <p>{{ level.id }}</p>
-                        </li>
-                        <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
-                        </li>
-                    </ul>
-                    <h2>Records</h2>
-                    <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
-                    <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
-                    <p v-else>This level does not accept new records.</p>
-                    <table class="records">
-                        <tr v-for="record in level.records" class="record">
-                            <td class="percent">
-                                <p>{{ record.percent }}%</p>
-                            </td>
-                            <td class="user">
-                                <a :href="record.link" target="_blank" class="type-label-lg">{{ record.user }}</a>
-                            </td>
-                            <td class="mobile">
-                                <img v-if="record.mobile" :src="\`/assets/phone-landscape\${store.dark ? '-dark' : ''}.svg\`" alt="Mobile">
-                            </td>
-                            <td class="hz">
-                                <p>{{ record.hz }}Hz</p>
-                            </td>
-                        </tr>
-                    </table>
-                </div>
+                <Level :level="level" :list="list" :key="level.rank" v-if="level && level.id!=0" />
+                <TierInfo :level="level" :list="list" :descending="descending" v-else-if="level?.id==0" />
                 <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
                     <p>(ノಠ益ಠ)ノ彡┻━┻</p>
                 </div>
             </div>
             <div class="meta-container">
                 <div class="meta">
-                    <div class="errors" v-show="errors.length > 0">
-                        <p class="error" v-for="error of errors">{{ error }}</p>
-                    </div>
-                    <div class="og">
-                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
-                    </div>
-                    <template v-if="editors">
-                        <h3>List Editors</h3>
-                        <ol class="editors">
-                            <li v-for="editor in editors">
-                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
-                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
-                                <p v-else>{{ editor.name }}</p>
-                            </li>
-                        </ol>
-                    </template>
-                    <h3>Submission Requirements</h3>
-                    <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
-                    </p>
-                    <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
-                    </p>
-                    <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
-                    </p>
-                    <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
-                    </p>
-                    <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
-                    </p>
-                    <p>
-                        Do not use secret routes or bug routes
-                    </p>
-                    <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
-                    </p>
-                    <p>
-                        Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
-                    </p>
+                    <Errors :errors="errors" />
+                    <TemplateDisclaimer />
+                    <hr class="divider">
+                    <Staff />
+                    <hr class="divider">
+                    <h3>Tags</h3>
+                    <p class="director" @click="search('⭐')">⭐ Rated</p>
+                    <p class="director" @click="search('✨')">✨ Subject to Exemptions</p>
+                    <p class="director" @click="search('💫')">💫 Accepted Under Old Standards</p>
+                    <p class="director" @click="search('🎖️')">🎖️ Creator Contest Winner</p>
+                    <p class="director" @click="search('❌')">❌ Pending Removal</p>
+                    <hr class="divider">
+                    <RecordRules />
+                    <hr class="divider">
+                    <DifficultyInfo />
+                    <hr class="divider">
+                    <CookiesDisclaimer />
                 </div>
             </div>
         </main>
     `,
+
     data: () => ({
-        list: [],
-        editors: [],
         loading: true,
-        selected: 0,
+        list: [],
+        staff: [],
         errors: [],
-        roleIconMap,
-        store
+        selected: 1,
+        store,
+        searchQuery: '',
+        sortOption: 0,
+        descending: true
     }),
+
+    methods: {
+        // used for the ability to deselect tag filters
+        search(query) {
+            if (this.searchQuery === query) {
+                this.searchQuery = '';
+            } else {
+                this.searchQuery = query;
+            }
+        },
+        selectFromParam() {
+            if (this.$route.params.level) {
+                const returnedIndex = this.list.findIndex(
+                    ([err, rank, lvl]) => 
+                        lvl.path === this.$route.params.level 
+                );
+                
+                if (returnedIndex === -1) this.errors.push(`The level ${this.$route.params.level} does not exist, please double check the URL.`);
+                else this.selected = returnedIndex;
+            }
+        },
+        scrollToSelected() {
+            this.$nextTick(() => {
+                const selectedElement = this.$refs.selected;
+                if (selectedElement && selectedElement[0] && selectedElement[0].firstChild) {
+                    selectedElement[selectedElement.length - 1].firstChild.scrollIntoView({ behavior: 'smooth', block: 'center' });
+                }
+            });
+        }
+    },
+
     computed: {
         level() {
-            return this.list[this.selected][0];
+            return this.list && this.list[this.selected] && this.list[this.selected][2];
         },
-        video() {
-            if (!this.level.showcase) {
-                return embed(this.level.verification);
+        filteredLevels() {
+            const query = this.searchQuery.toLowerCase();
+            let list = this.list
+            // this was a lot of fun!
+            let sortOption = parseInt(this.sortOption)
+
+            // use the separate indexing for searching Shenanigans
+            list = list.map((item, index) => ({ index, item }));
+            
+            // search logic
+            if (query.trim()) {
+                list = list.filter(({ item: [err, rank, level] }) =>
+                    (level?.name.toLowerCase())
+                        .includes(query) &&
+                    level?.id !== 0
+                )
             }
 
-            return embed(
-                this.toggledShowcase
-                    ? this.level.showcase
-                    : this.level.verification
-            );
+            // sort based on value of dropdown menu
+            if (sortOption === 1) {
+                list = list.filter(({ item }) =>
+                            item[2]?.id !== 0 &&
+                            averageEnjoyment(item[2]?.records) !== "?"
+                        )
+                    .sort((a, b) => {
+                            const enjoymentA = averageEnjoyment(a.item[2].records);
+                            const enjoymentB = averageEnjoyment(b.item[2].records);
+
+                            return enjoymentB - enjoymentA;
+                        })
+                        
+            } else if (sortOption === 2) {
+                list = list.filter(({ item }) =>
+                        item[2].id !== 0
+                    )
+                .sort((a, b) => {
+                    const recordLenA = a.item[2].records.length;
+                    const recordLenB = b.item[2].records.length;
+                    return recordLenB - recordLenA;
+                })
+                
+            }
+
+            // by default the list should be in descending order
+            if (!this.descending) {
+                list = list.reverse()
+            }
+
+            return list
         },
     },
+
     async mounted() {
-        // Hide loading spinner
-        this.list = await fetchList();
-        this.editors = await fetchEditors();
+        // Fetch list from store
+        this.list = this.store.list;
+        this.staff = store.staff;
+        
+        this.selectFromParam()
 
         // Error handling
         if (!this.list) {
             this.errors = [
-                "Failed to load list. Retry in a few minutes or notify list staff.",
+                'Failed to load list. Retry in a few minutes or notify list staff.',
             ];
         } else {
-            this.errors.push(
-                ...this.list
-                    .filter(([_, err]) => err)
-                    .map(([_, err]) => {
-                        return `Failed to load level. (${err}.json)`;
-                    })
-            );
-            if (!this.editors) {
-                this.errors.push("Failed to load list editors.");
+            this.store.errors.forEach((err) => 
+                this.errors.push(`Failed to load level. (${err}.json)`))
+
+            if (!this.staff) {
+                this.errors.push('Failed to load list staff.');
             }
         }
 
+        // Hide loading spinner
         this.loading = false;
+
+        // tests for incorrect difficulties and duplicate records
+        let i = 0
+        let currentdiff, newdiff;
+        while (i < this.list.length) {
+            if (this.list[i][2]) {
+                let templevel = this.list[i][2]
+
+                newdiff = templevel.difficulty 
+                if (templevel.id === 0) {
+                    if (templevel.difficulty !== currentdiff - 1 && currentdiff !== undefined) {
+                        console.error(`Found incorrect divider difficulty! Please set ${templevel.path}'s difficulty to ${currentdiff - 1}.`)
+                    }
+                    currentdiff = templevel.difficulty
+                }
+                
+                if (newdiff !== currentdiff) console.warn(`Found incorrect difficulty! ${templevel.name} (${templevel.path}.json) is set to ${newdiff}, please set it to ${currentdiff}.`)
+                
+                
+                const foundusers = []
+                for (const record of templevel.records) {
+                    if (record.enjoyment && (typeof record.enjoyment === "string" && record.enjoyment !== "?")) {
+                        console.warn(`Found wrong type of enjoyment on level ${templevel.name} (${record.enjoyment})! Please set enjoyment to a number or '?'`)
+                    }
+                    if (foundusers.includes(record.user) || record.user === templevel.verifier) {
+                        console.warn(`Found duplicate record! ${record.user} has a duplicate record on ${templevel.name} (${templevel.path}.json).`)
+                    } else {
+                        foundusers.push(record.user)
+                    }
+
+                    if (record.enjoyment && (templevel.creators.includes(record.user))) {
+                        console.warn(`Invalid enjoyment on ${templevel.name}: ${record.enjoyment}/10 by ${record.user}!`)
+                    }
+
+                }
+            }
+            i++
+        }
     },
-    methods: {
-        embed,
-        score,
+
+    watch: {        
+        store: {
+            handler(updated) {
+                this.list = updated.list;
+                this.staff = updated.staff;
+                updated.errors.forEach(err => {
+                    this.errors.push(`Failed to load level. (${err}.json)`);
+                })
+                this.selectFromParam()
+            }, 
+            deep: true
+        }
     },
 };
