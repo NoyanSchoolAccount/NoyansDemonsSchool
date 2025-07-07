672beff0c6ebdf7d3ca5a288d8186ffac87a71a0
Noyan <noyannowhasyt@gmail.com>
2025-07-07 17:40:21 +0300

    Update RecordRules.js

--- js/components/Sidebar/RecordRules.js
+++ js/components/Sidebar/RecordRules.js
@@ -12,17 +12,17 @@ export default {
                 The recording must have a previous attempt and death animation shown before the completion, unless the completion is on the first attempt.
             </p>
             <p>
-                The recording must show the player hit the end-wall as well as present your end stats, or the completion will be invalidated.
+                The recording must show the player hit the end-wall as well as the end screen.
             </p>
             <p>
                 Do not use secret routes, skips, or bug routes!
             </p>
             <p>
-                Cheat Indicator is required for all completions via MegaHack, Geode, or iCreate Pro. If you do not have Cheat Indicator on, your record will likely be invalidated (this is not 100% required for mobile as of yet due to mobile limitations).
-                If you are unsure which Geode mod menu to use, we recommend Eclipse.
+                Cheat Indicator is required for all completions via MegaHack, Eclipse, or iCreate Pro. If you do not have Cheat Indicator on, your record will likely be invalidated (this is not 100% required for mobile as of yet due to mobile limitations).
             </p>
             <p>
                 Records submitted through DMs will not be accepted. Video proof is required.
+            </p>
         </div>
     `
 }
