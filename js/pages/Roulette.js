a49ea301dfe6a762577220e41108414bb31c7394
Noyan <noyannowhasyt@gmail.com>
2025-07-25 18:26:52 +0300

    Update Roulette.js

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -18,11 +18,11 @@ export default {
                 <form class="options">
                     <div class="check">
                         <input type="checkbox" id="main" value="Main List" v-model="useMainList">
-                        <label for="main">Top 15 levels</label>
+                        <label for="main">Top 11 levels</label>
                     </div>
                     <div class="check">
                         <input type="checkbox" id="extended" value="Extended List" v-model="useExtendedList">
-                        <label for="extended">Levels below top 15</label>
+                        <label for="extended">Levels below top 11</label>
                     </div>
                     <Btn @click.native.prevent="onStart">{{ levels.length === 0 ? 'Start' : 'Restart'}}</Btn>
                 </form>
