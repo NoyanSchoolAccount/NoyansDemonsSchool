23a60c9e8d3d4fae5e9b8ef937cc9ddd8c939f84
Noyan <noyannowhasyt@gmail.com>
2025-07-25 18:13:48 +0300

    Create Website.js

---
+++ js/components/Sidebar/Website.js
@@ -0,0 +1,10 @@
+export default {
+    template: `
+        <h3>Website Link</h3>
+        <div class="right-text">
+            <p>
+            https://noyandemons.ps.fhgdps.com/dashboard
+            </p>
+        </div>
+    `
+}
