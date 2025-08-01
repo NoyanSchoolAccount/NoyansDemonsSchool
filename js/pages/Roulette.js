94a21cf2e47e7f675704e80354ef618e9be7db65
Noyan <noyannowhasyt@gmail.com>
2025-08-02 01:27:06 +0300

    Update Roulette.js

--- js/pages/Roulette.js
+++ js/pages/Roulette.js
@@ -18,11 +18,11 @@ export default {
                 <form class="options">
                     <div class="check">
                         <input type="checkbox" id="main" value="Main List" v-model="useMainList">
-                        <label for="main">Top 11 levels</label>
+                        <label for="main">Top 12 levels</label>
                     </div>
                     <div class="check">
                         <input type="checkbox" id="extended" value="Extended List" v-model="useExtendedList">
-                        <label for="extended">Levels below top 11</label>
+                        <label for="extended">Levels below top 12</label>
                     </div>
                     <Btn @click.native.prevent="onStart">{{ levels.length === 0 ? 'Start' : 'Restart'}}</Btn>
                 </form>
@@ -196,10 +196,10 @@ export default {
 
             const list = [];
             if (this.useMainList) {
-                list.push(...fullListMapped.slice(0, 11));
+                list.push(...fullListMapped.slice(0, 12));
             }
             if (this.useExtendedList) {
-                list.push(...fullListMapped.slice(11));
+                list.push(...fullListMapped.slice(12));
             }
 
             // Random 100 levels
