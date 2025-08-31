5b531d4397f2ad2329acfc79cae25b4cd0f8ce06
Noyan <noyannowhasyt@gmail.com>
2025-09-01 00:52:36 +0300

    Update Future.js

--- js/pages/Future.js
+++ js/pages/Future.js
@@ -2,27 +2,31 @@ import { embed } from '../util.js';
 
 document.title = "Future Levels";
 
-const container = document.createElement('div');
-container.className = 'future-levels-container';
-
-fetch('data/future_levels.json')
-  .then(res => res.json())
-  .then(levels => {
-    levels.forEach(level => {
-      const div = document.createElement('div');
-      div.className = 'future-level';
-
-      div.innerHTML = `
-        <img class="future-level-thumb" src="${level.thumbnail}" alt="${level.name} thumbnail">
+export default {
+  name: 'FuturePage',
+  data() {
+    return {
+      levels: []
+    };
+  },
+  created() {
+    fetch('data/future_levels.json')
+      .then(res => res.json())
+      .then(levels => {
+        this.levels = levels;
+      });
+  },
+  template: `
+    <div class="future-levels-container">
+      <div v-for="level in levels" :key="level.id" class="future-level">
+        <img class="future-level-thumb" :src="level.thumbnail" :alt="level.name + ' thumbnail'">
         <div class="future-level-info">
-          <h2>${level.name}</h2>
-          <p><strong>Best Percent:</strong> ${level.bestPercent}%</p>
-          <p><strong>Verifier(s):</strong> ${level.verifiers.join(', ')}</p>
-          <p><strong>Creator(s):</strong> ${level.creators.join(', ')}</p>
+          <h2>{{ level.name }}</h2>
+          <p><strong>Best Percent:</strong> {{ level.bestPercent }}%</p>
+          <p><strong>Verifier(s):</strong> {{ level.verifiers.join(', ') }}</p>
+          <p><strong>Creator(s):</strong> {{ level.creators.join(', ') }}</p>
         </div>
-      `;
-      container.appendChild(div);
-    });
-  });
-
-document.body.appendChild(container);
+      </div>
+    </div>
+  `
+};
