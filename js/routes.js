13d5b93f700ca2806339b861097672590b0cf11e
Noyan <noyannowhasyt@gmail.com>
2025-09-01 00:51:47 +0300

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -5,6 +5,7 @@ import Packs from './pages/Packs.js';
 import ChallengeList from './Archived/ChallengeList.js';
 import ChallengeLeaderboard from './Archived/ChallengeLeaderboard.js';
 import Grind from './pages/Grind.js'
+import FuturePage from './pages/Future.js';
 
 export default [
     // Sets the browser link to access each page
